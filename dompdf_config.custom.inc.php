410596f02d2b8e83f2cada5fa903e4e48aea5917
Kevin Langley <me@kevinlangleyjr.com>
2014-07-22 10:39:40 -0400

    changing to DOMPDF_FONT_CACHE to DOMPDF_FONT_DIR in dompdf custom config

--- dompdf_config.custom.inc.php
+++ dompdf_config.custom.inc.php
@@ -9,4 +9,4 @@ define( 'DOMPDF_ENABLE_REMOTE', true );
 define( 'DOMPDF_ENABLE_AUTOLOAD', false );
 define( 'DOMPDF_ENABLE_PHP', true );
 define( 'DOMPDF_LOG_OUTPUT_FILE', false );
-define( 'DOMPDF_FONT_CACHE', $upload_dir['basedir'] . '/dompdf/fonts' );
+define( 'DOMPDF_FONT_DIR', $upload_dir['basedir'] . '/dompdf/fonts' );

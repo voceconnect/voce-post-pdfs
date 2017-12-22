4d6a3d5af342324e8745408fc011cf0b80df60b2
Joe Hoyle <joe@humanmade.co.uk>
2017-12-21 19:26:39 -0500

    Store the dompdf configs in the temp dir

--- dompdf_config.custom.inc.php
+++ dompdf_config.custom.inc.php
@@ -1,12 +1,7 @@
 <?php
 
-$upload_dir = Voce_Post_PDFS::wp_upload_dir();
-if( ! is_dir( $upload_dir['basedir'] . '/dompdf/fonts' ) ){
-	mkdir( $upload_dir['basedir'] . '/dompdf/fonts', 0777, true );
-}
-
 define( 'DOMPDF_ENABLE_REMOTE', true );
 define( 'DOMPDF_ENABLE_AUTOLOAD', false );
 define( 'DOMPDF_ENABLE_PHP', true );
 define( 'DOMPDF_LOG_OUTPUT_FILE', false );
-define( 'DOMPDF_FONT_CACHE', $upload_dir['basedir'] . '/dompdf/fonts' );
+define( 'DOMPDF_FONT_CACHE', sys_get_temp_dir() );
